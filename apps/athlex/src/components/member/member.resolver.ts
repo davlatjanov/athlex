6dc972bef2c2d48068adeaddb6150f1b2de620bc
davlatjanov <ndavlatjonov@gmail.com>
2026-01-26 10:52:14 +0900

    feat: create member schema model

--- apps/athlex/src/components/member/member.resolver.ts
+++ apps/athlex/src/components/member/member.resolver.ts
@@ -1,4 +1,19 @@
 import { Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class MemberResolver {}
+export class MemberResolver {
+  public async signUp(): Promise<string> {
+    return 'User signed up';
+  }
+  public async login(): Promise<string> {
+    return 'User logged in';
+  }
+
+  public async updateMember(): Promise<string> {
+    return 'User updated';
+  }
+
+  public async getMember(): Promise<string> {
+    return 'User data';
+  }
+}
--- apps/athlex/src/components/member/member.service.ts
+++ apps/athlex/src/components/member/member.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(@InjectModel('Member') private memberModel: Model<null>) {}
+
+  public async signUp(): Promise<string> {
+    return 'User signed up';
+  }
+  public async login(): Promise<string> {
+    return 'User logged in';
+  }
+
+  public async updateMember(): Promise<string> {
+    return 'User updated';
+  }
+
+  public async getMember(): Promise<string> {
+    return 'User data';
+  }
+}
