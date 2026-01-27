9cadd9c5541646c2657c5a0dad775a1d3f5d9e1f
davlatjanov <ndavlatjonov@gmail.com>
2026-01-27 15:13:56 +0900

    feat: create like view follow comment DTOs

---
+++ apps/athlex/src/libs/dto/view/view.ts
@@ -0,0 +1,28 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
+import { ViewGroup } from '../../enums/view.enum';
+
+import { IsNotEmpty } from 'class-validator';
+import type { ObjectId } from 'mongoose';
+
+@ObjectType()
+export class View {
+	@Field(() => String)
+	_id: ObjectId;
+
+	@Field(() => ViewGroup)
+	viewGroup: ViewGroup;
+
+	@Field(() => String)
+	viewRefId: ObjectId;
+
+	@Field(() => String)
+	memberId: ObjectId;
+
+	@Field(() => Date)
+	createdAt: Date;
+
+	@Field(() => Date)
+	updatedAt: Date;
+}
