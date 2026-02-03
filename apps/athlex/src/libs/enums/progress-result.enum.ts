4258dc8df3b6fccba795caedb91cbfb3c51de01f
davlatjanov <ndavlatjonov@gmail.com>
2026-02-03 23:24:37 +0900

    fix: enum register error

--- apps/athlex/src/libs/enums/progress-result.enum.ts
+++ apps/athlex/src/libs/enums/progress-result.enum.ts
@@ -1,5 +1,11 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 // libs/enums/progress-result.enum.ts
 export enum ProgressResultStatus {
   ACTIVE = 'ACTIVE',
   DELETED = 'DELETED',
 }
+
+registerEnumType(ProgressResultStatus, {
+  name: 'ProgressResultStatus',
+});
